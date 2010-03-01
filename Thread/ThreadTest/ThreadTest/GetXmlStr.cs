30e437184e0f8a2bc91a6a5c65b7479483d80c6e
liketowindy@gmail.com <liketowindy@gmail.com@4cae45f4-b8dc-11de-b017-a9a2c7051f87>
2010-03-01 09:35:57 +0000

    对三级页面的抓取进行修改

--- Thread/ThreadTest/ThreadTest/GetXmlStr.cs
+++ Thread/ThreadTest/ThreadTest/GetXmlStr.cs
@@ -13,11 +13,14 @@ namespace ThreadTest
     {
         private static XmlDocument XmlDoc1;        
         private static IList<XmlNode> StringList;
-        private static string XmlStr;
+        private static Thread[] ThreadList;
+        private static int Num = 15;
+        private static int DieNum = 0;
+        private static StringBuilder XmlStr;
         public GetXmlStr()
         {
             //定义XmlDoc1
-            XmlDoc1 = CreateXmlDoc("ListUrl0226043253.xml");
+            XmlDoc1 = CreateXmlDoc("ListUrl0226043242.xml");
             //定义XmlDoc2
             //XmlDoc2 = CreateXmlDoc("ListUrl.xml");
             
@@ -25,24 +28,35 @@ namespace ThreadTest
             //root.RemoveAll();
             //定义StringList
             StringList = CreateStringList(XmlDoc1);
+            ThreadList = new Thread[Num];
+            for (int i = 0; i < Num; i++)
+            {
+                ThreadList[i] = new Thread(new ThreadStart(Run));
+                ThreadList[i].Name = i.ToString();
+            }
         }
         public static WaitCallback waitCallback = new WaitCallback(MyThreadWork);
 
         public void LStart()
         {
             GetXmlStr Pro = new GetXmlStr();
-            XmlStr += "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
-            XmlStr += "<docs>";
-            int MaxWorkerThreads = 25;
-            int MaxIOCompletionThreads = 2000;
-            int MinWorkerThreads = 1;
-            ThreadPool.GetMaxThreads(out MaxWorkerThreads, out MaxIOCompletionThreads);
-            ThreadPool.GetMinThreads(out MinWorkerThreads, out MaxIOCompletionThreads);
+            XmlStr = new StringBuilder();
+            XmlStr.Append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            XmlStr.Append("<docs>");
+            //int MaxWorkerThreads = 25;
+            //int MaxIOCompletionThreads = 2000;
+            //int MinWorkerThreads = 1;
+            //ThreadPool.GetMaxThreads(out MaxWorkerThreads, out MaxIOCompletionThreads);
+            //ThreadPool.GetMinThreads(out MinWorkerThreads, out MaxIOCompletionThreads);
             Console.WriteLine("开始" + DateTime.Now.ToString());
-            if (StringList.Count > 0)
-            {                
-                ThreadPool.QueueUserWorkItem(waitCallback, StringList[0]);                
-            }                
+            //if (StringList.Count > 0)
+            //{                
+            //    ThreadPool.QueueUserWorkItem(waitCallback, StringList[0]);                
+            //}  
+            for (int i = 0; i < Num; i++)
+            {
+                ThreadList[i].Start();
+            }
         }
         private static XmlDocument CreateXmlDoc(string fileName)
         {
@@ -62,21 +76,22 @@ namespace ThreadTest
             }
             return List;
         }
+        #region
         public static void MyThreadWork(object state)
         {
-            XmlStr += "<doc>";
+            XmlStr.Append("<doc>");
             XmlNode Node = (XmlNode)state;
             string url = Node.SelectSingleNode("doc_url").InnerText;
             Console.WriteLine("初始content" + DateTime.Now.ToString());
             GetContent content = new GetContent(url);
             Console.WriteLine("获得content" + DateTime.Now.ToString());
-            XmlStr += "<doc_url><![CDATA[" + Node.ChildNodes[0].InnerText + "]]></doc_url>";
-            XmlStr += "<doc_mySrcType><![CDATA[" + Node.ChildNodes[1].InnerText + "]]></doc_mySrcType>";
-            XmlStr += "<doc_MyDate><![CDATA[" + Node.ChildNodes[2].InnerText + "]]></doc_MyDate>";
-            XmlStr += "<doc_title><![CDATA[" + content.ArticleTitle + "]]></doc_title>";
-            XmlStr += "<doc_MySiteName><![CDATA[" + content.SiteName + "]]></doc_MySiteName>";
-            XmlStr += "<doc_content><![CDATA[" + content.ArticleContent + "]]></doc_content>";
-            XmlStr += "</doc>";
+            XmlStr.Append("<doc_url><![CDATA[" + Node.ChildNodes[0].InnerText + "]]></doc_url>");
+            XmlStr.Append("<doc_mySrcType><![CDATA[" + Node.ChildNodes[1].InnerText + "]]></doc_mySrcType>");
+            XmlStr.Append("<doc_MyDate><![CDATA[" + Node.ChildNodes[2].InnerText + "]]></doc_MyDate>");
+            XmlStr.Append("<doc_title><![CDATA[" + content.ArticleTitle + "]]></doc_title>");
+            XmlStr.Append("<doc_MySiteName><![CDATA[" + content.SiteName + "]]></doc_MySiteName>");
+            XmlStr.Append("<doc_content><![CDATA[" + content.ArticleContent + "]]></doc_content>");
+            XmlStr.Append("</doc>");
             //WriteXml(_UrlList);
             StringList.RemoveAt(0);
             Console.WriteLine("赋值XmlStr" + DateTime.Now.ToString());
@@ -86,15 +101,63 @@ namespace ThreadTest
             }
             else
             {
-                XmlStr += "</docs>";
-                XmlDoc1.LoadXml(XmlStr);
+                XmlStr.Append("</docs>");
+                XmlDoc1.LoadXml(XmlStr.ToString());
                 string path = System.Environment.CurrentDirectory.ToString();
                 path = path.Replace("bin\\Debug", "contet\\") + "ListUrl0226043253.xml";
                 XmlDoc1.Save(path);
                 Console.WriteLine(DateTime.Now.ToString());
                 Console.WriteLine("三级页面链接抓取完毕！");
-            }            
-            
+            }
+
+        }
+        #endregion
+        private void Run()
+        {
+            int Index = -1;
+            while (true)
+            {
+                if (Index == -1)
+                {
+                    Index = Convert.ToInt32(Thread.CurrentThread.Name);
+                }
+                if (Index < StringList.Count)
+                {
+                    try
+                    {
+                        XmlNode Node = (XmlNode)StringList[Index];
+                        string url = Node.SelectSingleNode("doc_url").InnerText;
+                        Console.WriteLine(Thread.CurrentThread.Name + "初始content" + DateTime.Now.ToString() + "Index:" + Index.ToString());
+                        GetContent content = new GetContent(url);
+                        Console.WriteLine(Thread.CurrentThread.Name + "获得content" + DateTime.Now.ToString() + "Index:" + Index.ToString());
+                        XmlStr.Append("<doc>");
+                        XmlStr.Append("<doc_url><![CDATA[" + Node.ChildNodes[0].InnerText + "]]></doc_url>");
+                        XmlStr.Append("<doc_mySrcType><![CDATA[" + Node.ChildNodes[1].InnerText + "]]></doc_mySrcType>");
+                        XmlStr.Append("<doc_MyDate><![CDATA[" + Node.ChildNodes[2].InnerText + "]]></doc_MyDate>");
+                        XmlStr.Append("<doc_title><![CDATA[" + content.ArticleTitle + "]]></doc_title>");
+                        XmlStr.Append("<doc_MySiteName><![CDATA[" + content.SiteName + "]]></doc_MySiteName>");
+                        XmlStr.Append("<doc_content><![CDATA[" + content.ArticleContent + "]]></doc_content>");
+                        XmlStr.Append("</doc>");
+                        Index = Index + Num;
+                    }
+                    catch { }
+                }
+                else if (Index >= StringList.Count)
+                {
+                    DieNum = DieNum + 1;
+                    if (DieNum == Num)
+                    {
+                        XmlStr.Append("</docs>");
+                        XmlDoc1.LoadXml(XmlStr.ToString());
+                        string path = System.Environment.CurrentDirectory.ToString();
+                        path = path.Replace("bin\\Debug", "contet\\") + "ListUrl0226043242.xml";
+                        XmlDoc1.Save(path);
+                        Console.WriteLine(DateTime.Now.ToString());
+                        Console.WriteLine("三级页面链接抓取完毕！");
+                    }
+                    Thread.CurrentThread.Abort();
+                }               
+            }
         }
     }
 }
